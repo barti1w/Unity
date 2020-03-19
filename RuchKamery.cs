c15eb9070bb1e01305f266aa339a3290d80def92
barti1w <57442061+barti1w@users.noreply.github.com>
2020-03-19 20:26:01 +0100

    Kamera

---
+++ RuchKamery.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RuchKamery : MonoBehaviour
+{
+    public Transform Gracz;    
+    public Vector3 Perspektywa;
+    private Vector3 PozycjaGracza;
+    void Start()
+    {
+        PozycjaGracza = Gracz.position;
+    }
+
+   
+    void Update()
+    {
+        transform.position = PozycjaGracza + Perspektywa;
+    }
+}
